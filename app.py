076fa1d4957d4b728355968ff09b4498c5837b4b
vishwanathreddy3345 <vishwanathreddy3345@gmail.com>
2026-03-20 12:48:41 +0530

    added db initialization

--- app.py
+++ app.py
@@ -184,10 +184,9 @@ def uploaded_file(filename):
 
 # ================= RUN =================
 
-
-
-if __name__ == "__main__":
-    with app.app_context():
+with app.app_context():
     db.create_all()
     print("✅ Database created successfully")
+
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=port)
